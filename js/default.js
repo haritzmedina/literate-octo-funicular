b27b00ed9e68b19271200936ca175dfeefaaa140
Haritz Medina <pirque07@gmail.com>
2015-05-24 16:56:10 +0200

    Colorful equalizer first example.

--- js/default.js
+++ js/default.js
@@ -10,7 +10,36 @@ Visualization = function(){
 Visualization.prototype.initialize = function(){
     // Create the canvas where the visualization will be
     this.createVisualizationCanvas();
+    // Create the analyzer context
+    this.createAnalyzer();
+    // Testing purposes
     this.test();
+};
+
+Visualization.prototype.createAnalyzer = function(){
+    // Create the audio analyser
+    var context = new AudioContext();
+    var audio = document.getElementById('audio');
+    var audioSrc = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+    audioSrc.connect(analyser);
+    analyser.connect(context.destination);
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    //
+    var me = this;
+    //
+    function renderFrame() {
+        window.requestAnimationFrame(renderFrame);
+        // Stop if the song is stopped
+        if (audio.paused == true) {
+            return;
+        }
+        // Update data in frequencyData
+        analyser.getByteFrequencyData(frequencyData);
+        // Render frame based on values in frequencyData
+        me.render(frequencyData);
+    }
+    renderFrame();
 };
 
 Visualization.prototype.createVisualizationCanvas = function () {
@@ -49,8 +78,12 @@ Visualization.prototype.start = function () {
         // Render frame based on values in frequencyData
         me.render(frequencyData);
     }
-    audio.play();
     renderFrame();
+};
+
+Visualization.prototype.play = function(){
+    var audio = document.getElementById('audio');
+    audio.play();
 };
 
 Visualization.prototype.render = function(frequencyData){
@@ -63,9 +96,23 @@ Visualization.prototype.render = function(frequencyData){
     // Columns
     for(var i=0; i<canvas.width;i++){
         var val = frequencyData[i];
-        // Rows
-        for(var j=0;j<val*2;j++){
-            //var pos = ((i*1024)+j)*4;
+        var j;
+        // First 30% rows blue
+        for(j=0;j<val*0.6;j++){
+            var pos = ((canvas.height-j)*canvas.width+i)*4;
+            data[pos] = 100;// Red
+            data[pos+1] = 100;// Green
+            data[pos+2] = 200;// Blue
+            data[pos+3] = 255;// Alpha
+        }
+        for(j;j<val*1.2;j++){
+            var pos = ((canvas.height-j)*canvas.width+i)*4;
+            data[pos] = 175;// Red
+            data[pos+1] = 100;// Green
+            data[pos+2] = 150;// Blue
+            data[pos+3] = 255;// Alpha
+        }
+        for(j;j<val*2;j++){
             var pos = ((canvas.height-j)*canvas.width+i)*4;
             data[pos] = 255;// Red
             data[pos+1] = 100;// Green
