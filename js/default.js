0f767e1972331608a534cb7a9b8a753765a0322c
Haritz Medina <pirque07@gmail.com>
2015-05-26 12:06:10 +0200

    Refactored initial code. Other minimum changes.

--- js/default.js
+++ js/default.js
@@ -2,32 +2,33 @@
  * Created by Haritz on 5/12/2015.
  */
 
-Visualization = function(){
-    this.initialize();
-};
-
-
-Visualization.prototype.initialize = function(){
-    // Create the canvas where the visualization will be
-    this.createVisualizationCanvas();
+Analyzer = function(){};
+Analyzer.prototype.initialize = function(audio, visualization){
+    this.visualization = visualization;
     // Create the analyzer context
-    this.createAnalyzer();
+    this.createAnalyzer(audio);
     // Testing purposes
     this.test();
 };
 
-Visualization.prototype.createAnalyzer = function(){
+Analyzer.prototype.updateVisualization = function(visualization){
+    this.visualization = visualization;
+};
+
+Analyzer.prototype.test = function(){
+
+};
+
+Analyzer.prototype.createAnalyzer = function(audio){
     // Create the audio analyser
     var context = new AudioContext();
-    var audio = document.getElementById('audio');
     var audioSrc = context.createMediaElementSource(audio);
-    var analyser = context.createAnalyser();
-    audioSrc.connect(analyser);
-    analyser.connect(context.destination);
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    //
+    var analyzer = context.createAnalyser();
+    audioSrc.connect(analyzer);
+    analyzer.connect(context.destination);
+    //var frequencyData = new Uint8Array(analyser.frequencyBinCount);
+    var frequencyData = new Float32Array(analyzer.frequencyBinCount);
     var me = this;
-    //
     function renderFrame() {
         window.requestAnimationFrame(renderFrame);
         // Stop if the song is stopped
@@ -35,11 +36,20 @@ Visualization.prototype.createAnalyzer = function(){
             return;
         }
         // Update data in frequencyData
-        analyser.getByteFrequencyData(frequencyData);
+        analyzer.getFloatFrequencyData(frequencyData);
+        console.log(frequencyData);
         // Render frame based on values in frequencyData
-        me.render(frequencyData);
+        me.visualization.render(frequencyData);
     }
     renderFrame();
+};
+
+Visualization = function(){};
+Visualization.prototype.initialize = function(){
+    // Create the canvas where the visualization will be
+    this.createVisualizationCanvas();
+    // Testing purposes
+    this.test();
 };
 
 Visualization.prototype.createVisualizationCanvas = function () {
@@ -54,31 +64,11 @@ Visualization.prototype.test = function(){
 
 };
 
-Visualization.prototype.start = function () {
-    // Create the audio analyser
-    var context = new AudioContext();
-    var audio = document.getElementById('audio');
-    var audioSrc = context.createMediaElementSource(audio);
-    var analyser = context.createAnalyser();
-    audioSrc.connect(analyser);
-    analyser.connect(context.destination);
-    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    //
-    var me = this;
-    //
-    function renderFrame() {
-        window.requestAnimationFrame(renderFrame);
-        // Stop if the song is stopped
-        if (audio.paused == true) {
 
-            return;
-        }
-        // Update data in frequencyData
-        analyser.getByteFrequencyData(frequencyData);
-        // Render frame based on values in frequencyData
-        me.render(frequencyData);
-    }
-    renderFrame();
+Visualization.prototype.clear = function(){
+    var canvas = document.getElementById("visualization");
+    var context = canvas.getContext("2d");
+    context.clearRect ( 0 , 0 , canvas.width, canvas.height);
 };
 
 Visualization.prototype.play = function(){
@@ -90,29 +80,29 @@ Visualization.prototype.render = function(frequencyData){
     var canvas = document.getElementById("visualization");
     var context = canvas.getContext("2d");
     // Clear previous draw
-    context.clearRect ( 0 , 0 , canvas.width, canvas.height );
+    this.clear();
     var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     var data = imageData.data;
     // Columns
     for(var i=0; i<canvas.width;i++){
-        var val = frequencyData[i];
+        var val = (frequencyData[i]+100)*4;
         var j;
         // First 30% rows blue
-        for(j=0;j<val*0.6;j++){
+        for(j=0;j<val*0.3;j++){
             var pos = ((canvas.height-j)*canvas.width+i)*4;
             data[pos] = 100;// Red
             data[pos+1] = 100;// Green
             data[pos+2] = 200;// Blue
             data[pos+3] = 255;// Alpha
         }
-        for(j;j<val*1.2;j++){
+        for(j;j<val*0.6;j++){
             var pos = ((canvas.height-j)*canvas.width+i)*4;
             data[pos] = 175;// Red
             data[pos+1] = 100;// Green
             data[pos+2] = 150;// Blue
             data[pos+3] = 255;// Alpha
         }
-        for(j;j<val*2;j++){
+        for(j;j<val;j++){
             var pos = ((canvas.height-j)*canvas.width+i)*4;
             data[pos] = 255;// Red
             data[pos+1] = 100;// Green
@@ -122,3 +112,31 @@ Visualization.prototype.render = function(frequencyData){
     }
     context.putImageData(imageData, 0, 0);
 };
+
+var MusicVisualization = MusicVisualization || {
+        analyzer : new Analyzer(),
+        visualization : new Visualization(),
+        initialize : function(){
+            var audio = document.getElementById('audio');
+            this.visualization.initialize();
+            this.analyzer.initialize(audio, this.visualization);
+
+        },
+        play : function(){
+            var audio = document.getElementById('audio');
+            audio.load();
+            audio.play();
+        },
+        pause : function(){
+            var audio = document.getElementById('audio');
+            audio.pause();
+        },
+        nextVisualization : function(){
+
+        },
+        previousVisualization : function(){
+
+        }
+    };
+
+MusicVisualization.initialize();
